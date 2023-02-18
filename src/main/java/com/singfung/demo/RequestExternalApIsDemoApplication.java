e1c19f8a16e5dafbc532823eeb3b2a701ac3ea31
sing-fung <1587898817@qq.com>
2023-02-18 14:43:34 -0500

    first commit

---
+++ src/main/java/com/singfung/demo/RequestExternalApIsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.singfung.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestExternalApIsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestExternalApIsDemoApplication.class, args);
+    }
+
+}
